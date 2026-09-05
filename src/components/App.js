b8ffae3c5deb41976fb411fe8a60c5a0e71240de
Elyse-Joyeux <elysejoyeux590@gmail.com>
2026-09-05 14:05:15 +0200

    chat

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import Login from './Login';
 import Signup from './Signup';
+import Chats from './Chats'
+import { AuthProvider } from '../contexts/AuthContext'
 
 function App() {
   return (
     <Router>
-      <Switch>
+      <AuthProvider>
+        <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
-        <Redirect to="/login" />
-      </Switch>
+        <Route exact path='/chats' component={Chats} />
+        <Redirect to="/chats" />
+        </Switch>
+      </AuthProvider>
     </Router>
   );
 }
